fd659b6332a52fe739a215ec22ea4aadd73f0841
fbv000 <99373777+fbv000@users.noreply.github.com>
2024-02-23 13:11:28 -0300

    Create monitor.py

---
+++ monitor.py
@@ -0,0 +1,26 @@
+from bs4 import BeautifulSoup
+import requests
+
+# URL of the dashboard page
+url = 'https://ocean.xyz/stats/37SN2dyo9xWdJ3owHafLvUNLAPkexxXHZy.default'
+
+# Fetch the page content
+response = requests.get(url)
+page_content = response.text
+
+# Parse the HTML content
+soup = BeautifulSoup(page_content, 'html.parser')
+
+for row in soup.find_all('tr', class_='table-row'):
+    # Assuming the worker's name is in the first td
+    name_link = row.find('td', class_='hide-overflow')
+    name = name_link.text.strip() if name_link else 'Unknown Worker'
+    
+    status_div = row.find('div', class_='status-online-text')
+    # Check if status_div exists before accessing .text
+    status = status_div.text.strip() if status_div else 'Offline'
+    if name != "Unknown Worker" and name != "Total":   
+        if status == 'Offline':
+            print(f"{name} is currently offline.")
+        else:
+            print(f"{name} is online.")
